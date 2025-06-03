7b5395c62388a570d8bfa27faca5be1fce07673c
JishnuTP <tpjishnu5@gmail.com>
2025-06-03 11:41:51 +0530

    package

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const userController= require('../controllers/userController');  
 const router = express.Router();
+const userController= require('../controllers/userController');  
+
 
 router.get('/events',userController.getEvents )
 router.get('/events/:id', userController.getEventById);
