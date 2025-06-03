5e912707a68fa700b5ff1aa4c6438b347b8f48f9
JishnuTP <tpjishnu5@gmail.com>
2025-06-03 11:46:54 +0530

    base url

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,5 +1,7 @@
 // API Base URL
-export const API_BASE_URL = 'http://localhost:5000/api';
+// export const API_BASE_URL = 'http://localhost:5000/api';
+
+export const API_BASE_URL ="https://mc-merrys-mufasa-ibhe.vercel.app/api"
 
 // Auth Endpoints
 export const AUTH_ENDPOINTS = {
