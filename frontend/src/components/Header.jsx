8cd7e59b1969d4d676e81f3d246e010e02cb9642
SHARON-MK <sharonmkvalayam@gmail.com>
2025-06-17 00:49:37 +0530

    logo updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../public/logo/mlogo-min.png';
+import logo from '../../public/logo/mymasters-logo.png';
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false);
